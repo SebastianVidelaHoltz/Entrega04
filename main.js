e3d57f5ec6bc4d09d04362cd7dda479bec42031b
Sebastian Videla Holtz <sebasvidelaholtz@gmail.com>
2023-05-22 22:22:29 -0300

    Realicé correciones

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
 // Sebastian Videla Holtz
 // Función para convertir USD a ARS
 
-function convertirUSDaARS(cantidadUSD, tipoCambio, precioDolar) {
-    precioDolar=470
-    tipoCambio="Peso argentino".toLowerCase()
-    return cantidadUSD * precioDolar;
-  }
-  
-  // Solicitar al usuario la cantidad de USD a convertir y el tipo de cambio actual
-  let cantidadUSD = parseFloat(prompt("Ingrese la cantidad de USD a convertir en pesos argentinos:"));
-  let tipoCambio = prompt("Ingrese el tipo de cambio actual:".toLowerCase());
-  
-  // Validar que las entradas sean números válidos
+function convertirUSDaARS(cantidadUSD) {
+  const precioDolar = 470;
+  return cantidadUSD * precioDolar;
+}
+
+// Solicitar al usuario la cantidad de USD a convertir
+function solicitarCantidadUSD() {
+  let cantidadUSD = parseFloat(
+    prompt('Ingrese la cantidad de USD a convertir en pesos argentinos:')
+  );
+
   while (isNaN(cantidadUSD)) {
-    cantidadUSD = parseFloat(prompt("Ingrese una cantidad de USD válida:"));
+    cantidadUSD = parseFloat(prompt('Ingrese una cantidad de USD válida:'));
   }
-  
-  // Convertir USD a ARS
-  let cantidadARS = convertirUSDaARS(cantidadUSD, tipoCambio);
-  
-  // Mostrar el resultado
-//   console.log("$" + cantidadUSD + " USD equivale a " + cantidadARS + " ARS")
-  alert("$" + cantidadUSD + " USD equivale a " + cantidadARS + " ARS")
-  
 
+  return cantidadUSD;
+}
 
+// Convertir USD a ARS y mostrar el resultado
+function convertirYMostrarResultado(cantidadUSD) {
+  const cantidadARS = convertirUSDaARS(cantidadUSD);
+  alert('$' + cantidadUSD + ' USD equivale a ' + cantidadARS + ' ARS');
+}
 
-
-
-
+// Ejecutar el programa
+const cantidadUSD = solicitarCantidadUSD();
+convertirYMostrarResultado(cantidadUSD);
 
 
 
