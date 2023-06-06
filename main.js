d3cd046f5dfd9307a9a7f004d9a81235a84d1288
Sebastian Videla Holtz <sebasvidelaholtz@gmail.com>
2023-06-05 23:44:16 -0300

    Agregue divisas

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 // Sebastian Videla Holtz
-// Función para convertir USD a ARS
+// Definimos objetos de divisas con nombre y precio
+const divisas = [
+  { nombre: 'Dolar', precio: 485 },
+  { nombre: 'Yuan', precio: 35 },
+  { nombre: 'Yen', precio: 1.80 }
+];
 
+// Función para convertir una cantidad de USD a pesos argentinos
 function convertirUSDaARS(cantidadUSD) {
-  const precioDolar = 470;
-  return cantidadUSD * precioDolar;
+  const precioDolar = divisas.find(d => d.nombre === 'Dolar')?.precio;
+  return precioDolar ? cantidadUSD * precioDolar : null;
+}
+
+// Función para convertir una cantidad de divisa (Yen o Yuan) a pesos argentinos
+function convertirDivisaAARS(divisa, cantidadDivisa) {
+  const precioDivisa = divisas.find(d => d.nombre === divisa)?.precio;
+  return precioDivisa ? cantidadDivisa * precioDivisa : null;
 }
 
 // Solicitar al usuario la cantidad de USD a convertir
 function solicitarCantidadUSD() {
-  let cantidadUSD = parseFloat(
-    prompt('Ingrese la cantidad de USD a convertir en pesos argentinos:')
-  );
+  let cantidadUSD = parseFloat(prompt('Ingrese la cantidad de USD a convertir:'));
 
   while (isNaN(cantidadUSD)) {
     cantidadUSD = parseFloat(prompt('Ingrese una cantidad de USD válida:'));
@@ -19,16 +29,37 @@ function solicitarCantidadUSD() {
   return cantidadUSD;
 }
 
-// Convertir USD a ARS y mostrar el resultado
-function convertirYMostrarResultado(cantidadUSD) {
-  const cantidadARS = convertirUSDaARS(cantidadUSD);
-  alert('$' + cantidadUSD + ' USD equivale a ' + cantidadARS + ' ARS');
+// Solicitar al usuario la cantidad de divisa a convertir
+function solicitarCantidadDivisa(divisa) {
+  let cantidadDivisa = parseFloat(prompt(`Ingrese la cantidad de ${divisa} a convertir:`));
+
+  while (isNaN(cantidadDivisa)) {
+    cantidadDivisa = parseFloat(prompt(`Ingrese una cantidad de ${divisa} válida:`));
+  }
+
+  return cantidadDivisa;
+}
+
+// Función de orden superior para convertir y mostrar el resultado
+function convertirYMostrarResultado(divisa) {
+  return function(cantidad) {
+    const cantidadARS = convertirDivisaAARS(divisa, cantidad);
+    if (cantidadARS) {
+      alert(`${cantidad} ${divisa} equivale a ${cantidadARS} ARS`);
+    } else {
+      alert(`La cantidad de ${divisa} ingresada no es válida`);
+    }
+  };
 }
 
 // Ejecutar el programa
 const cantidadUSD = solicitarCantidadUSD();
-convertirYMostrarResultado(cantidadUSD);
+const seleccionarDivisa = prompt('Seleccione una divisa: Dolar, Yuan o Yen').trim();
+const divisaEncontrada = divisas.find(d => d.nombre.toLowerCase() === seleccionarDivisa.toLowerCase());
 
-
-
-
+if (divisaEncontrada) {
+  const convertirDivisa = convertirYMostrarResultado(divisaEncontrada.nombre);
+  convertirDivisa(cantidadUSD);
+} else {
+  alert('La divisa seleccionada no es válida');
+}
