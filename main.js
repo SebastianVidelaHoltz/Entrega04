ca8ba7528e1effb657f70a5e3fc9a26084c1fe67
SebasVidelaHoltz <sebasvidelaholtz@gmail.com>
2023-07-08 18:46:57 -0300

    Agregue libreria y promesas

--- main.js
+++ main.js
@@ -4,36 +4,35 @@ const resultado = document.getElementById('resultado');
 const montoInput = document.getElementById('monto');
 const monedaOrigenSelect = document.getElementById('monedaOrigen');
 
-// Definir objetos de divisas con nombre y precio
-const divisas = [
-  { nombre: 'USD', precio: 485 },
-  { nombre: 'YUAN', precio: 35 },
-  { nombre: 'YEN', precio: 1.80 }
-];
+async function getDivisas() {
+  try {
+    const response = await fetch("divisas.json");
+    const divisas = await response.json();
+    return divisas;
+  } catch (error) {
+    console.error("Error", error);  
+  }
+}
 
 // Función para convertir una cantidad de divisa a pesos argentinos
-function convertirDivisaAARS(divisa, cantidadDivisa) {
-  const divisaObj = divisas.find(d => d.nombre === divisa);
+async function convertirDivisaAARS(divisa, cantidadDivisa) {
+  const data = await getDivisas()
+  const divisaObj = data.divisas.find(d => d.nombre === divisa);
   return cantidadDivisa * divisaObj.precio;
 }
 
-// Función para mostrar el resultado de la conversión
-function mostrarResultado(resultadoConversión) {
-  resultado.textContent = 'El resultado de la conversión es: ARS ' + resultadoConversión;
-}
-
 // Evento click del botón de conversión
-btnConvertir.addEventListener('click', function() {
+btnConvertir.addEventListener('click', async function() {
   const monto = parseFloat(montoInput?.value);
   const monedaOrigen = monedaOrigenSelect.value;
   
-  // Realizar la conversión 
-  const resultadoConversión = convertirDivisaAARS(monedaOrigen, monto);
+// Realizar la conversión 
+  const resultadoConversión = await convertirDivisaAARS(monedaOrigen, monto);
   
-  // Mostrar el resultado de la conversión en el elemento <span>
-  mostrarResultado(resultadoConversión);
+  Swal.fire(`El resultado de la conversión es: ARS ${resultadoConversión}`)
+
 });
 // Funciones para localeStorage y JSON
-const divisasJSON = JSON.stringify(divisas);
+const divisasJSON = JSON.stringify(getDivisas());
 localStorage.setItem('divisasData', divisasJSON);
-const divisasConvertidas = localStorage.getItem(divisas)
+const divisasConvertidas = localStorage.getItem('divisasData')
